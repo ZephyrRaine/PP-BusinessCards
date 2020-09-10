65b825beacd257cdd8ad3933e3bde85ee5a160f3
Jocelyn <jocelyn.ailloud@lecole-ldlc.com>
2020-09-10 17:07:48 +0200

    ok

---
+++ Assets/Scenes/ImageTracking/Scripts/LKD.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LKD : MonoBehaviour
+{
+    public void OpenURL(string url)
+    {
+        Application.OpenURL(url);
+        
+    }
+
+    public void EnvoyerSignal(string button)
+    {
+        if (button == "calendar")
+        {
+            
+        }else if (button=="mic")
+        {
+            
+        }  
+    }
+}
---
+++ Assets/ShowDebug.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+[ExecuteInEditMode]
+public class ShowDebug : MonoBehaviour
+{
+public RectTransform parent;
+public TMP_Text text;
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "x : "+parent.localScale.x+" \n y : " + parent.localScale.y;
+    }
+}
